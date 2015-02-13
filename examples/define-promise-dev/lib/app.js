4144450c556f661a5e423752f2419ebeff5e762a
Mehran Hatami <mehranhatamie@gmail.com>
2015-02-13 21:42:23 +0330

    modify example to compare co and definejs

--- examples/define-promise-dev/lib/app.js
+++ examples/define-promise-dev/lib/app.js
@@ -1,7 +1,12 @@
-define(function* (exports, module) {
+define(function * (exports, module) {
 
   var utils = yield require('utils'),
+    definejsSLN = yield require('definejs-sln'),
+    coSLN = yield require('co-sln'),
     $ = yield require('../vendor/jquery');
+
+  console.log('definejsSLN>', definejsSLN);
+  console.log('coSLN>', coSLN);
 
   var app = {
     body: $('body').get(0),
@@ -10,6 +15,6 @@ define(function* (exports, module) {
       console.log('App just got lunched!:' + this.body);
     }
   };
-  
+
   module.exports = app;
 });
