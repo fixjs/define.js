59ff7b5632af12bb2c115b8536b9542ad9267553
Mehran Hatami <mehranhatamie@gmail.com>
2015-02-21 20:55:26 +0330

    new tests and a temp change in npm test

--- test/testSuite.promise.js
+++ test/testSuite.promise.js
@@ -4,21 +4,7 @@
   var specs = [
     './spec/var/emptyArray',
     './spec/var/doc',
-    './spec/var/info',
-    './spec/utils',
-    './spec/baseInfo',
-    './spec/utils.execute',
-    './spec/setup',
-    './spec/setup.regular',
-    './spec/setup.promised',
-    './spec/utils.getFileName',
-    './spec/utils.getUrl',
-    './spec/utils.createScript',
-    './spec/utils.getScript',
-    './spec/loader',
-    './spec/amd',
-    // './spec/amd.define.patterns',
-    './spec/define'
+    './spec/var/info'
   ];
 
   if(!QUnit.config.karmaIsInCharge){
