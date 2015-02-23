ca179e07021b6724de67b218906deba73c9681f7
Mehran Hatami <mehranhatamie@gmail.com>
2015-02-23 09:58:38 +0330

    tests for utils and createScript

--- test/spec/utils.createScript.js
+++ test/spec/utils.createScript.js
@@ -1,28 +1,65 @@
 define(function () {
   'use strict';
 
+  var $ = global.jQuery;
+
   function testCreateScript(assert, utils, info, doc, baseInfo) {
 
     assert.strictEqual(typeof utils.getUrl, 'function', 'utils.getUrl is a dependency');
 
     var callback = sinon.stub(),
       errorCallback = sinon.stub(),
-      el,
+      el = $('<script>', {
+        src: 'lib/testModule.js'
+      }).get(0),
       origType = info.options.scriptType,
       origBaseElement = baseInfo.baseElement,
-      url = 'lib/testModule';
+      url = 'lib/testModule',
+      callbackArg0,
+      callbackArg1;
+
+    assert.stub(document, 'createElementNS')
+      .withArgs('http://www.w3.org/1999/xhtml', 'script')
+      .returns(el);
+    assert.stub(document, 'createElement')
+      .withArgs('script')
+      .returns(el);
+
+    // assert.stub(el, 'attachEvent');
+    assert.stub(el, 'addEventListener');
+    assert.stub(el, 'removeEventListener');
+
+    // utils.createScript.withArgs('lib/testModule', callback, callback).returns(el);
 
     info.options.scriptType = undefined;
     baseInfo.baseElement = undefined;
 
     el = utils.createScript(url, callback, errorCallback);
-    assert.equal(typeof el, 'object', 'utils.createScript works ...');
 
-    assert.equal(typeof el.nodeName, 'string', 'utils.createScript works ...');
-    assert.equal(el.nodeName.toLowerCase(), 'script', 'utils.createScript works ...');
+    assert.ok(el.addEventListener.calledTwice, 'el.addEventListener for load and error');
 
-    assert.equal(typeof el.tagName, 'string', 'utils.createScript works ...');
-    assert.equal(el.tagName.toLowerCase(), 'script', 'utils.createScript works ...');
+    callbackArg0 = el.addEventListener.getCall(0).args[1];
+    callbackArg1 = el.addEventListener.getCall(1).args[1];
+    assert.strictEqual(typeof callbackArg0, 'function', 'callbackArg0 is a function');
+    assert.strictEqual(typeof callbackArg1, 'function', 'callbackArg1 is a function');
+
+    var e = {
+      currentTarget: el,
+      type: 'load'
+    };
+
+    // el.readyState = 'complete' or el.readyState = 'loaded'
+    callbackArg0(e);
+
+    assert.ok(callback.calledOnce, 'callback gets called with event callback');
+    assert.ok(callback.calledWith('success'), 'callback gets called with event callback');
+    assert.ok(el.removeEventListener.calledOnce, 'el.removeEventListener gets called to remove the load event');
+
+    callbackArg1(e);
+
+    assert.ok(errorCallback.calledOnce, 'callback gets called with event callback:error');
+    assert.ok(errorCallback.calledWith('error'), 'callback gets called with event callback:error');
+    assert.ok(el.removeEventListener.calledTwice, 'el.removeEventListener gets called to remove the load event:error');
 
     assert.strictEqual(el.async, true, 'utils.createScript works ...');
     assert.strictEqual(el.type, 'text/javascript', 'utils.createScript works ...');
@@ -31,6 +68,7 @@ define(function () {
     info.options.scriptType = 'my-own-datatype/javascript';
 
     el = utils.createScript(url, callback, errorCallback);
+
     assert.strictEqual(el.type, 'my-own-datatype/javascript', 'utils.createScript reads dataType from options ...');
 
     //tests for baseElement
@@ -38,13 +76,16 @@ define(function () {
     info.options.scriptType = origType;
     baseInfo.baseElement = origBaseElement;
 
-    //more DOM tests
+    // el.attachEvent.restore();
+    el.addEventListener.restore();
+    document.createElementNS.restore();
+    document.createElement.restore();
   }
 
   fix.test('utils.createScript', {
     message: 'utils.createScript works as a helper utils functions',
     module: {
-      beforeEach: function (assert) {
+      beforeEach: function () {
         fix.stubInsertAndAppend();
       },
       afterEach: function () {
