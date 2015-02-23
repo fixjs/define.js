db4ed700ab06a3487fec7144fccae251e46a03f0
Mehran Hatami <mehranhatamie@gmail.com>
2015-02-23 10:48:52 +0330

    test async rejections

--- test/spec/async.js
+++ test/spec/async.js
@@ -25,13 +25,28 @@ define(function () {
     });
   }
 
+  function asyncRejectedPromiseFN(async) {
+    return async.Promise(function * (fulfill, reject) {
+      var str = yield getPromisedString();
+      str = 'This is just a ' + str;
+      reject(str);
+      return str;
+    });
+  }
+
   fix.test('async', {
     message: 'async module provides a means to deal with function generators',
     resolver: function (assert, async) {
       var asyncValue = asyncFN(async)(),
-        asyncPromiseValue = asyncPromiseFN(async);
+        asyncPromiseValue = asyncPromiseFN(async),
+        rejectedPromise;
 
-      return Promise.all([asyncValue, asyncPromiseValue]);
+      rejectedPromise = asyncRejectedPromiseFN(async)
+        .catch(function (str) {
+          return 'ERROR:' + str;
+        });
+
+      return Promise.all([asyncValue, asyncPromiseValue, rejectedPromise]);
     }
   }).then(function (assert, async, promisedArray) {
 
@@ -41,11 +56,27 @@ define(function () {
 
     assert.ok($.isArray(promisedArray), 'both promised values are present');
 
-    assert.strictEqual(promisedArray.length, 2, 'promisedArray has two member as we expect');
+    assert.strictEqual(promisedArray.length, 3, 'promisedArray has two member as we expect');
 
     assert.strictEqual(promisedArray[0], 'This is just a simple promised string!', 'async works as expected!');
 
     assert.strictEqual(promisedArray[0], promisedArray[1], 'async and async.Promise both work as expected!');
+
+    assert.strictEqual(promisedArray[2], 'ERROR:' + promisedArray[0], 'async handles rejection and exceptions!');
+
+    var genFN,
+      asyncGen1,
+      asyncGen2;
+
+    genFN = function * () {
+      var str = yield 'str';
+      return str;
+    };
+
+    asyncGen1 = async(genFN);
+    asyncGen2 = async(genFN);
+
+    assert.strictEqual(asyncGen1, asyncGen2, 'async caches the async alternative of function generators!');
 
   });
 });
