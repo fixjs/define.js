787f5f4c433047eaaf2fc640c3e334ce287d7ea6
Mehran Hatami <mehranhatamie@gmail.com>
2015-03-01 13:19:54 +0330

    test baseInfo with base element in page

--- test/spec/baseInfo.js
+++ test/spec/baseInfo.js
@@ -2,15 +2,33 @@ define(function () {
   'use strict';
 
   function testBaseInfo(assert, baseInfo, doc) {
-    var head = doc.head || doc.getElementsByTagName('head')[0],
-      base = doc.getElementsByTagName('base')[0];
-    
+    var head = doc.head || doc.getElementsByTagName('head')[0];
+
+    //First test run on baseInfo
+    var baseEl = $('<base>', {
+      href: '/baseUrl'
+    }).appendTo(baseInfo.head);
+
+    assert.currentScript = undefined;
+
+    baseInfo.baseUrl = '/testBase';
+
     baseInfo();
 
-    if (baseInfo.baseElement) {
-      assert.strictEqual(baseInfo.baseElement, base, 'baseInfo.baseElement stores the correct value');
-      head = baseInfo.baseElement.parentNode;
-    }
+    assert.strictEqual(baseInfo.baseElement, baseEl.get(0), 'baseInfo.baseElement stores the correct value');
+    equal(baseInfo.head, baseInfo.baseElement.parentNode, 'baseInfo.head stores the correct value:with base element in page');
+    assert.strictEqual(baseInfo.baseUrl, '', 'baseInfo.baseUrl is empty when there is no currentScript');
+
+    baseEl.remove();
+
+    //second test run on baseInfo
+    assert.currentScript = $('<script>', {
+      'src': 'spec/baseInfo.js',
+      'global': 'GLOB',
+      'base': '../spec/'
+    }).get(0);
+
+    baseInfo();
 
     equal(baseInfo.head, head, 'baseInfo.head  stores the correct value');
 
@@ -21,21 +39,15 @@ define(function () {
   fix.test('baseInfo', {
     message: 'is a expose function for AMD functions and more DefineJS attributes',
     module: {
-      beforeEach: function () {
-        var currentScript = $('<script>', {
-          'src': 'spec/baseInfo.js',
-          'global': 'GLOB',
-          'base': '../spec/'
-        }).get(0);
-
+      beforeEach: function (assert) {
         Object.defineProperty(document, 'currentScript', {
           get: function () {
-            return currentScript;
+            return assert.currentScript;
           },
           configurable: true
         });
       },
-      afterEach: function () {
+      afterEach: function (assert) {
         delete document.currentScript;
       }
     },
@@ -43,7 +55,7 @@ define(function () {
   }).then(function (assert, baseInfo, doc) {
 
     assert.strictEqual(typeof baseInfo, 'function', 'baseInfo is an object');
-    
+
     assert.strictEqual(doc, document, 'doc is the global document object');
 
     testBaseInfo(assert, baseInfo, doc);
