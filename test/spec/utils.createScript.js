aff9cfdbb7e475af27a5bb4b2ce2dd79bb51caca
Mehran Hatami <mehranhatamie@gmail.com>
2015-03-01 14:20:56 +0330

    add attachEvent/detachEvent tests

--- test/spec/utils.createScript.js
+++ test/spec/utils.createScript.js
@@ -17,6 +17,64 @@ define(function () {
     document.createElement.restore();
   }
 
+  function testCreateScriptWithAttach(assert, utils) {
+    var callback = sinon.stub(),
+      errorCallback = sinon.stub(),
+      el = $('<script>', {
+        src: 'lib/testModule2.js'
+      }).get(0),
+      url = 'lib/testModule2',
+      callbackArg0,
+      origOpera = global.opera,
+      attachEventSupport = $.isFunction(el.attachEvent);
+
+    global.opera = undefined;
+
+    if (attachEventSupport) {
+      assert.stub(el, 'attachEvent');
+      assert.stub(el, 'detachEvent');
+    } else {
+      el.attachEvent = sinon.stub();
+      el.detachEvent = sinon.stub();
+    }
+
+    stubCreateElement(assert, el);
+    el = utils.createScript(url, callback, errorCallback);
+    restoreCreateElement();
+
+    assert.ok(el.attachEvent.calledOnce, 'el.attachEvent called with onreadystatechange');
+    assert.strictEqual(el.attachEvent.getCall(0).args[0], 'onreadystatechange', 'el.attachEvent adds the callback to onreadystatechange event queue');
+
+    callbackArg0 = el.attachEvent.getCall(0).args[1];
+
+    assert.strictEqual(typeof callbackArg0, 'function', 'callbackArg0 is a function');
+
+    var e = {
+      currentTarget: el
+    };
+    el.readyState = 'complete'; //el.readyState = 'loaded'
+
+    callbackArg0(e);
+
+    assert.ok(callback.calledOnce, 'callback gets called with event callback');
+    assert.ok(callback.calledWith('success'), 'callback gets called with event callback');
+
+    assert.ok(el.detachEvent.calledOnce, 'el.detachEvent gets called to remove the load event');
+    assert.strictEqual(el.detachEvent.getCall(0).args[0], 'onreadystatechange', 'el.detachEvent removes the callback from onreadystatechange event queue');
+
+    assert.strictEqual(errorCallback.callCount, 0, 'errorCallback doesnt get called when attachEvent exist, the whole flow actually works with readyState');
+
+    assert.strictEqual(el.async, true, 'utils.createScript works ...');
+    assert.strictEqual(el.type, 'text/javascript', 'utils.createScript works ...');
+    assert.strictEqual(el.charset, 'utf-8', 'utils.createScript works ...');
+
+    if (attachEventSupport) {
+      el.attachEvent.restore();
+      el.detachEvent.restore();
+    }
+    global.opera = origOpera;
+  }
+
   function testCreateScript(assert, utils, info, doc, baseInfo) {
 
     assert.strictEqual(typeof utils.getUrl, 'function', 'utils.getUrl is a dependency');
@@ -121,7 +179,10 @@ define(function () {
   }).then(function (assert, utils, info, doc, baseInfo) {
 
     assert.strictEqual(typeof utils.createScript, 'function', 'utils.createScript is a function');
+
     testCreateScript(assert, utils, info, doc, baseInfo);
+
+    testCreateScriptWithAttach(assert, utils);
 
     global.utils = utils;
 
